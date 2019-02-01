cf274250eef142dd54ed1c2289f753ba2efe7f78
Aymeric <aymeric.cousaert@insa-lyon.fr>
2019-02-01 16:12:34 +0100

    clean + top10 wip

--- ligne.h
+++ ligne.h
@@ -1,11 +1,91 @@
-#ifndef LIGNE_H
+#if ! defined ( LIGNE_H )
 #define LIGNE_H
+#include <string>
+using namespace std;
 
+
+//--------------------------------------------------- Interfaces utilisées
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <Ligne>
+//
+//
+//------------------------------------------------------------------------
 
 class Ligne
 {
+    //----------------------------------------------------------------- PUBLIC
+    
 public:
-    Ligne();
+    //----------------------------------------------------- Méthodes publiques
+    // type Méthode ( liste des paramètres );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+    static string decompose(char sep, string uneLigne);
+    void afficher();
+    string getCible();
+    string getReferer();
+    
+    
+    //------------------------------------------------- Surcharge d'opérateurs
+    Ligne & operator = ( const Ligne & unLigne );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+    
+    
+    //-------------------------------------------- Constructeurs - destructeur
+    Ligne ( const Ligne & unLigne );
+    // Mode d'emploi (constructeur de copie) :
+    //
+    // Contrat :
+    //
+    
+    Ligne (string uneLigne);
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+    
+    virtual ~Ligne ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+    
+    //------------------------------------------------------------------ PRIVE
+    
+protected:
+    //----------------------------------------------------- Méthodes protégées
+    
+    //----------------------------------------------------- Attributs protégés
+    
+    string ip;
+    string logName;
+    string authenticatedUser;
+    int jour;
+    string mois;
+    int annee;
+    int heure;
+    int minute;
+    int seconde;
+    string decalage;
+    string action;
+    string cible;
+    int status;
+    string donnees;
+    string referer;
+    string navigateur;
+    
 };
+
+//-------------------------------- Autres définitions dépendantes de <Ligne>
 
 #endif // LIGNE_H
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <list>
 #include <map>
+using namespace std;
 
 #include "Ligne.h"
 
@@ -16,7 +17,6 @@ struct informations
 
 map<string, informations> mapCibles;
 
-
 informations remplissage(Ligne& uneLigne)
 {
     informations infos;
@@ -29,68 +29,95 @@ informations remplissage(Ligne& uneLigne)
 void AfficherTop10()
 {
     int min = 2147483647;
+    int indMin = 0;
     string top[10];
     int i = 0;
     for (map<string, informations>::iterator it = mapCibles.begin(); it != mapCibles.end(); ++it)
     {
         if (i < 10)
-        {
             top[i] = it->first;
-            if (it->second.nbApparition < min)
-                min = it->second.nbApparition;
-        }
-        else
-            if (it->second.nbApparition > min) // On remplace le plus petit
+        // On calcule le min
+        for (int m = 0; m < 10; m++)
+        {
+            if (mapCibles[top[m]].nbApparition < min)
             {
-                for (int j = 0; j < 10; j++)
+                min = mapCibles[top[m]].nbApparition;
+                indMin = m;
+            }
+        }
+        if (i >= 10)
+        {
+            // On remplace le min il y a besoin
+            cout << "min : " << min << endl;
+            cout << "indice min : " << indMin << endl;
+            cout << it->second.nbApparition << endl;
+            if (it->second.nbApparition > min)
+            {
+                top[indMin] = it->first;
+                cout << it->first << endl << endl;
+                // On recalcule le min
+                for (int m = 0; m < 10; m++)
                 {
-                    if (mapCibles[top[j]].nbApparition == min)
-                        top[j] = it->first;
-                }
-                //recalcule du min
-                min = top[0];
-                for (int j = 1; j < 10; j++)
-                {
-                    if (mapCibles[top[j]].nbApparition < min)
-                        min = mapCibles[top[j]].nbApparition ;
+                    if (mapCibles[top[m]].nbApparition < min)
+                    {
+                        min = mapCibles[top[m]].nbApparition;
+                        indMin = m;
+                    }
                 }
             }
-
-
-        cout << it->first << " " << it->second.nbApparition << endl;
-
+            
+        }
+        
+        // cout << it->first << " " << it->second.nbApparition << endl;
         i++;
     }
+    for (int m = 0; m < 10; m++)
+        cout << top[m] << endl;
+    cout << endl;
+    // On tri le top obtenu
+    string topTrie[10];
+    // Tableau qui permet de selectionner à quelles cibles on a encore accès (on va les éliminer au fur et à mesure que le Top 10 se remplit)
+    int acces[10] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
     int max = 0;
-    for (int j = 0; j < 10; j++) {
-        int id =0;
+    for (int m = 0; m < 10; m++)
+        cout << acces[m] << " ";
+    cout << endl;
+    for (int j = 0; j < 10; j++)
+    {
         max = 0;
+        int id = 0;
         for (int k = 0; k < 10; k++)
         {
-            if (mapCibles[top[j]].nbApparition > max)
+            if (acces[k] == 1 && mapCibles[top[k]].nbApparition > max)
             {
-                max = mapCibles[top[j]].nbApparition;
+                max = mapCibles[top[k]].nbApparition;
                 id = k;
             }
         }
-        cout << top[id] << "  " << top[id].nbApparition;
-        top[id] = 0;
-}
-
-
+        acces[id] = 0;
+        topTrie[j] = top[id];
+        for (int m = 0; m < 10; m++)
+            cout << acces[m] << " ";
+        cout << endl;
+    }
+    cout << "Voici le Top 10 :" << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << i + 1 << ". " << topTrie[i] << " avec " << mapCibles[topTrie[i]].nbApparition << " accès" << endl;
+    }
 }
 
 int main(int argc, char* argv[])
 {
 
     string ligne;
-    ifstream fichier("Test.log",ios::in);
+    ifstream fichier("Test.log", ios::in);
     if (fichier)
     {
         while (getline(fichier, ligne))
         {
             Ligne l1 = Ligne(ligne);
-             l1.afficher();
+            // l1.afficher();
             if (mapCibles.find(l1.getCible()) == mapCibles.end())
             {
                 informations infos = remplissage(l1);
@@ -108,6 +135,13 @@ int main(int argc, char* argv[])
             }
         }
         fichier.close();
+        
+        for (map<string, informations>::iterator it = mapCibles.begin(); it != mapCibles.end(); ++it)
+        {
+            cout << it->first << " \t" << it->second.nbApparition << endl;
+        }
+        cout << endl;
+        cout << endl;
         AfficherTop10();
     }
     else cout << "Fichier inexistant" << endl;
